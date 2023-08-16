ce58ca15caf120316b2d2cbec4e04a364b9d8afd
Nickolas Pedri Miranda <nickmirandabr@gmail.com>
2023-08-16 14:32:41 -0500

    finished exercises and updated functions

--- prepare.py
+++ prepare.py
@@ -23,6 +23,19 @@ def prep_titanic(df):
     return df
 
 
+def titanic_dummies(df):
+    df = pd.get_dummies(df, columns=['sex'], drop_first=True)
+    df = pd.get_dummies(df, columns=['class', 'embark_town'])
+    return df
+
+
+def titanic():
+    df = ac.get_titanic_data()
+    impute_df(df, 'age', strategy='median')
+    df = prep_titanic(df)
+    return df
+
+
 def prep_telco(df):
     telco = df.drop(columns=['payment_type_id', 'internet_service_type_id', 'contract_type_id', 'customer_id'])
     telco['total_charges'] = telco['total_charges'].astype(float)
@@ -32,12 +45,6 @@ def prep_telco(df):
                            drop_first=True)
     telco = pd.get_dummies(telco, columns=['contract_type', 'internet_service_type', 'payment_type'])
     return telco
-
-
-def titanic_dummies(df):
-    df = pd.get_dummies(df, columns=['sex'], drop_first=True)
-    df = pd.get_dummies(df, columns=['class', 'embark_town'])
-    return df
 
 
 def split_x_y(df, target=''):
@@ -50,13 +57,6 @@ def impute_df(df, column='', strategy='mean'):
     imputer = SimpleImputer(strategy=strategy)
     imputer.fit(df[[column]])
     df[[column]] = imputer.transform(df[[column]])
-    return df
-
-
-def titanic():
-    df = ac.get_titanic_data()
-    impute_df(df, 'age', strategy='median')
-    df = prep_titanic(df)
     return df
 
 
@@ -78,13 +78,13 @@ def baseline(df, data='', target_value='', show_results=True):
     df.drop(columns=['baseline'], inplace=True)
 
 
-
-def evaluate(df, data='', model='Dataframe', target='', show_results=True):
+def evaluate_model(dataframe, data='data column', model='Dataframe', p_class='', show_results=True):
+    df = dataframe
     df['model'] = model
     m_acc = (df[data] == df['model']).mean()
-    sub_rec = df[df[data] == target]  # Subset of all positive cases for recall
+    sub_rec = df[df[data] == p_class]  # Subset of all positive cases for recall
     m_rec = (sub_rec[data] == sub_rec['model']).mean()
-    s_pre = df[df['model'] == target]  # Subset of all positive guesses
+    s_pre = df[df['model'] == p_class]  # Subset of all positive guesses
     m_pre = (s_pre[data] == s_pre['model']).mean()
     if show_results:
         print(f'Model accuracy is: {round(m_acc * 100, 2)}%.')
@@ -92,7 +92,17 @@ def evaluate(df, data='', model='Dataframe', target='', show_results=True):
         print(f'Model precision is: {round(m_pre * 100, 2)}%.')
 
 
+''' dataframe is the dataframe with all of the information.
+    data is the column name of where the data is under.
+    model is the dataframe containing model information
+    p_class is the variable that you are trying to predict'''
+
+
 def importance(training, model):
     imp = {'cols': training.columns,
            'importance': model.feature_importances_}
     return pd.DataFrame(imp).sort_values(by='importance', ascending=False)
+
+
+''' training is the training data (x_train)
+    model is the actual model object'''
